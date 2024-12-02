7bb2ec6cbc7f8d08fc2e2f1b1b0f40da7362ca89
Demian <demian@gitaddremote.com>
2024-12-01 22:21:02 -0500

    Added scraping tool

--- src/utils/scrapingTools.ts
+++ src/utils/scrapingTools.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { logger } from logger
 
 export async function scrapeValueFromDiv(url: string, selector: string): Promise<string> {
     try {
