651c6863dd71b06ae4560993534e6fcf37b48a2b
gitaddremote <demian@gitaddremote.com>
2025-04-16 16:18:18 -0400

    ISSUE-15: More cleanup

--- src/utils/discord-rest-client.ts
+++ src/utils/discord-rest-client.ts
@@ -8,8 +8,6 @@ const logger = getLogger();
 const DISCORD_API_VERSION = '10';
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
                           
-logger.info('*********>>>>>>> DISCORD_BOT_TOKEN:', process.env.DISCORD_BOT_TOKEN);
-
 if (!DISCORD_BOT_TOKEN) {
   logger.error('Missing DISCORD_BOT_TOKEN in environment.');
   throw new Error('DISCORD_BOT_TOKEN must be set in the environment.');
